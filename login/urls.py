d4d1655faaf59a2d12e65f92b95d5f5359d9c9ae
nurdtechie98 <nurdtechie98@gmail.com>
2017-07-04 01:37:34 +0530

    returns home once logged in,without url extension

--- login/urls.py
+++ login/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'login/', views.login, name = 'login'),
 	url(r'home/$',views.home, name='home'),
+	url(r'^$',views.home, name='home'),
 	url(r'logout/$',views.logout,name='logout'),
 ]
