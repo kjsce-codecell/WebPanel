e3485ed99ce9cb817d0648920fca151c48f802b3
Chaitya Shah <Chaitya62@gmail.com>
2017-07-07 13:55:39 +0530

    remove quiz app

--- quiz/README.md
+++
@@ -1,1 +0,0 @@
-<h1 align ="center" > Feedback Form  App </h1>
--- quiz/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	url(r'^$', views.index, name = 'Index'),
-	url(r'^save/$', views.success, name = 'Success'),
-	url(r'^ranklist/$', views.weeks, name = "Weeks"),
-	url(r'^ranklist/week(?P<week>[0-9]+)/$', views.ranks, name = 'Ranks'),
-]
