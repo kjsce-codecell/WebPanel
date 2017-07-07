8e814cb015c472b8beefd6ed4b582289342a0113
Ankit Tripathi <ankitmani.t@somaiya.edu>
2017-07-07 14:59:31 +0530

    Add fill form. Results coming.

--- feedbackform/admin.py
+++ feedbackform/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Form, Question, Choice
 
-# Register your models here.
+admin.site.register(Form)
+admin.site.register(Question)
+admin.site.register(Choice)
