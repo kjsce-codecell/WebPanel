e83df30b26ec7322af1b7ca4d831e46fe929c1c0
Ankit Tripathi <Ankitmani.t@somaiya.edu>
2017-07-07 15:54:11 +0530

    Add fill form and results to Feedback app
    * Remove Pycache
    
    * Fix import
    
    * Add Create Feedback
    
    * Add fill form. Results coming.
    
    * Update landing_page.html
    
    * Update landing_page.html

--- feedbackform/urls.py
+++ feedbackform/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^create/$', views.create, name = 'Create'),
 	url(r'^save/$', views.save, name = 'Save'),
-]
+	url(r'^form(?P<id>[0-9]+)/$', views.fill, name = 'Fill'),
+	url(r'^submit/$', views.submit, name = 'Submit'),
--- feedbackform/views.py
+++ feedbackform/views.py
@@ -3,9 +3,47 @@ from django.http import HttpResponse
 import json
 from .models import Form, Question, Choice
 
-# Create your views here.
-def index(request):
-	return HttpResponse("Hello World")
+def fill(request, id):
+	form = {}
+	f = Form.objects.all().filter(id = id)
+	try:
+		form['name'] = f[0].name
+		form['id'] = f[0].id
+	except:
+		return redirect('/home')
+	q_set = f[0].question_set.all()
+	form['questions'] = []
+	for i,q in enumerate(q_set):
+		question = {}
+		question['text'] = q.q_text
+		question['type'] = q.q_type
+		question['no'] = i+1
+		if q.q_type is not 1:
+			question['choices'] = []
+			ch = q.choice_set.all()
+			for choice in ch:
+				question['choices'].append(choice.c_text)
+		form['questions'].append(question)
+	return render(request, 'feedbackform/index.html', {"title": "Fill Form", "fillForm": True, "data": form})
+
+def submit(request):
+	if request.method == "POST":
+		f = []
+		try:
+			f = Form.objects.all().get(id = request.POST.get('id', 0))
+		except:
+			return HttpResponse("Error: Form Doesn't Exist")
+		for i in range (f.question_set.all().count()):
+			if not (request.POST.get(str(i+1), '-1') == '-1' or request.POST.get(str(i+1), '-1') == ''):
+				c = f.question_set.all()[i].choice_set.filter(c_text = request.POST.get(str(i+1), '-1'))
+				if len(c) == 1:
+					c[0].c_count = c[0].c_count + 1
+					c[0].save()
+				else:
+					f.question_set.all()[i].choice_set.create(c_text = request.POST.get(str(i+1), '-1'), c_count = 1)
+		return HttpResponse("Success")
+	else:
+		return redirect("/home/")
 
 def save(request):
 	if request.method == "POST":
