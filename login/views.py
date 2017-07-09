841abb23b43191a697d3284cd36ab3ba77c64f12
VinitraMk <vinitramk@gmail.com>
2017-07-09 15:58:59 +0530

    object_404 import in login.views

--- login/views.py
+++ login/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from .forms import LoginForm
 from .models import Login
