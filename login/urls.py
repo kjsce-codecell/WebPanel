24e3f9c43a6767d504c97d41be9398938aa425f0
VinitraMk <vinitramk@gmail.com>
2017-07-09 16:03:39 +0530

    exclude words regex in login.urls

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	url(r'login/', views.login, name = 'login'),
 	url(r'home/$',views.home, name='home'),
 	url(r'logout/$',views.logout,name='logout'),
-        url(r'^(?P<shortId>[-\w]{1,7})$',views.redirectOriginal,name='redirectOriginal'),
+    url(r'^(?!admin|home|login|home)(?P<shortId>[-\w]{1,7})$',views.redirectOriginal,name='redirectOriginal'),
 ]
