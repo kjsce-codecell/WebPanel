d469ec95b70b542a509e87e1653b7bb09839a6ee
VinitraMk <vinitramk@gmail.com>
2017-07-09 15:56:12 +0530

    removed redirectOriginal from shortener.views

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import *
 from . import views
 urlpatterns=[
         url(r'^$',views.index,name='index'),
-        url(r'^(?P<shortId>[-\w]{1,7})$',views.redirectOriginal,name='redirectOriginal'),
+        #url(r'^(?P<shortId>[-\w]{1,7})$',views.redirectOriginal,name='redirectOriginal'),
         url(r'^shortenUrl/$',views.shortenUrl,name='shortenUrl'),
        ] 
